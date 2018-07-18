de897384bd60a5200eade5263ff4cc90ebe6325f
LaMar Lofton <lamarlofton55@gmail.com>
2018-07-18 09:10:10 -0500

    learn how npm is like a app store for javascript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const $ = require('jquery');
-let sayHello = () => {
-    console.log('Hello');
-}
-
-
-sayHello();
-
 
 $('body').html("This is me, modifiying the DOM");
+
+
+let sayHello = name => console.log(`Hello ${name}`);
+
+sayHello('LaMar');
+
+
